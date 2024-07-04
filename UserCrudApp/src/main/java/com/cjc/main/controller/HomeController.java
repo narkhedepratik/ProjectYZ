290120269b44839a41f643b4de1f495f2dc7362f
nilulilhare <nilulilhare732@gmail.com>
2024-07-04 15:42:11 +0530

    Save User Data

--- UserCrudApp/src/main/java/com/cjc/main/controller/HomeController.java
+++ UserCrudApp/src/main/java/com/cjc/main/controller/HomeController.java
@@ -1,8 +1,21 @@
 package com.cjc.main.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cjc.main.model.UserDetails;
 
 @RestController
 public class HomeController {
+	
+	@PostMapping("/saveUserData")
+	public String SaveDataUser(@RequestBody UserDetails u)
+	{
+		System.out.println("Hello");
+		return "SaveData";
+	}
 
 }
