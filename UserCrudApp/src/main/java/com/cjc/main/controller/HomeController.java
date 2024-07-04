b815c49011ca72c84aa634bbe04bdac31366a0d2
narkhedepratik <pratiknarkhede039@gmail.com>
2024-07-04 15:53:49 +0530

    get users Api

--- UserCrudApp/src/main/java/com/cjc/main/controller/HomeController.java
+++ UserCrudApp/src/main/java/com/cjc/main/controller/HomeController.java
@@ -1,21 +1,34 @@
 package com.cjc.main.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cjc.main.model.UserDetails;
 
+
 @RestController
 public class HomeController {
 	
+
 	@PostMapping("/saveUserData")
 	public String SaveDataUser(@RequestBody UserDetails u)
 	{
 		System.out.println("Hello");
 		return "SaveData";
+	}
+	@GetMapping("/users")
+	public ResponseEntity<List<UserDetails>> getAllUsers()
+	{
+		return new ResponseEntity<>(null,HttpStatus.OK);
+
 	}
 
 }
