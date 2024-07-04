8f8468b20e781f81508961473f64759aa411d4c7
narkhedepratik <pratiknarkhede039@gmail.com>
2024-07-04 15:23:38 +0530

    UserCrud app initial commit

---
+++ UserCrudApp/src/main/java/com/cjc/main/model/UserDetails.java
@@ -0,0 +1,42 @@
+package com.cjc.main.model;
+
+public class UserDetails {
+
+	private int userId;
+	private String userNmae;
+	private int userAge;
+	private String userEmail;
+	private String password;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserNmae() {
+		return userNmae;
+	}
+	public void setUserNmae(String userNmae) {
+		this.userNmae = userNmae;
+	}
+	public int getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
